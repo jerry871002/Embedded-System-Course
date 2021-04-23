0989274985a36e2d307a90bd531f7465ac0c2f70
Jerry Yang <jerry871002@gmail.com>
2021-04-23 14:03:58 +0800

    Lab7 finished

--- Lab7/quiz1/camera_pi.py
+++ Lab7/quiz1/camera_pi.py
@@ -23,15 +23,36 @@ class Camera(object):
             PROP_FRAME_WIDTH = cv2.CAP_PROP_FRAME_WIDTH
             PROP_FRAME_HEIGHT = cv2.CAP_PROP_FRAME_HEIGHT
 
-        self.video = PiVideoStream().start()
+        # video source
+        self.video = cv2.VideoCapture(0)
+        self.video.set(PROP_FRAME_WIDTH, 320)
+        self.video.set(PROP_FRAME_HEIGHT, 240)
+
+        cascPath = "../Lecture7_facial camera/model/haarcascade_frontalface_default.xml"
+        self.faceCascade = cv2.CascadeClassifier(cascPath)
     
     def __del__(self):
-        self.video.stop()
+        self.video.release()
     
     def get_frame(self):
-        while True:
-            image = self.video.read()
-            if image is not None:
-                break
+        ret, image = self.video.read()
+
+        # make the image gray scale
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+        # detect faces
+        faces = self.faceCascade.detectMultiScale(
+            gray,
+            scaleFactor=1.1,
+            minNeighbors=5,
+            minSize=(30, 30)
+        )
+
+        print (f'Found {len(faces)} faces!')
+
+        # Draw a rectangle around the faces
+        for (x, y, w, h) in faces:
+            cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
         ret, jpeg = cv2.imencode('.jpg', image)
         return jpeg.tostring()
